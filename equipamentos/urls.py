9afd2bbdf5db2c4273a90aeae3379eecaa0c0207
Eric_Augustin <ericaugustin@hotmail.com>
2023-01-20 22:01:32 -0300

    implementado formulario de visialização de equipamentos e detalhes dos equipamentos

--- equipamentos/urls.py
+++ equipamentos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path("",views.home, name="home"),
-               path('listarEquipamentos/',views.lista_equipamentos, name='listar_equipamentos')
-    
-    
+               path('listarEquipamentos/',views.lista_equipamentos, name='listar_equipamentos'),
+               path('exibirDetalheEquipamento/',views.exibirDetalheEquipamento, name='exibirDetalheEquipamento'),
+               
+               
 ]
--- equipamentos/views.py
+++ equipamentos/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import Usuario,Tipo,Equipamento
+from .models import Usuario,Tipo,Equipamento,Material_consumo
 from django.shortcuts import redirect 
 from hashlib import sha256
 import re
 def home(request):
+    if not request.session.get('usuario'):
+        return redirect('/auth/login/?status=2')
     # cria a view do login do usuário
     status=str(request.GET.get('status'))
     return render(request, "home.html", {'status':status})
+
 def lista_equipamentos(request):
+    if not request.session.get('usuario'):
+        return redirect('/auth/login/?status=2')
     equipamentos=Equipamento.objects.all()
     return render(request, "exibirEquipamentos.html", {'equipamentos':equipamentos})
+
+def exibirDetalheEquipamento(request):
+    if not request.session.get('usuario'):
+        return redirect('/auth/login/?status=2')
+    id=str(request.GET.get('id'))
+    equipamento=Equipamento.objects.get(id=id)
+    materiais=Material_consumo.objects.filter(equipamento__id=id)
+    return render(request, "exibirDetalheEquipamento.html", {'equipamento':equipamento, 'materiais':materiais})
