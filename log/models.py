91a7d8a1b35108e0cbc91e8c865b1ce40430fed1
Eric Augsutin <ericaugustin@hotmail.com>
2023-02-01 13:56:03 -0300

    criando model log

--- log/models.py
+++ log/models.py
@@ -4,7 +4,12 @@ from equipamentos.models import Equipamento
 from notas.models import Nota_equipamento
 
 class Log(models.Model):
-    transacao=models.CharField(max_length=30)
+    lista_transacoes=[('eq','Equipamento'),('te','Tipo Equipamento'),('fn','Fornecedor'),('li','Local Instalação'),('mc','Material Consumo'),
+                        ('me','media'),('dc','Disciplina de Manutenção'),('mf','Modo de Falha'),('me','Modo de falha Equipamento'),
+                        ('nm','Nota Material'),('ne','Nota Equipamento'),('us','usuario'),('tu','Tipo de Usuario')]
+    lista_movimentos=[('cd','Cadastro'),('lt','Listagem'),('ed','Edição'),('dl','Delete')]
+    transacao=models.CharField(choices=lista_transacoes,max_length=10)
+    movimento=models.CharField(choices=lista_movimentos,max_length=10)
     data_cadastro=models.DateTimeField(auto_now=True, auto_now_add=False)
     usuario=models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
     equipamento=models.ForeignKey(Equipamento,on_delete=models.DO_NOTHING, null=True)
