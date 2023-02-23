4ce411a0a2122de19aa3258ef5137590bb4c8458
Eric Augsutin <ericaugustin@hotmail.com>
2023-02-23 13:53:33 -0300

    criando forms notas

---
+++ notas/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.forms import *
+from .models import *
+from django.forms import modelform_factory
+from django.forms import BaseModelFormSet
+from cadastro_equipamentos.settings import TIME_ZONE
+from equipamentos.models import *
+import datetime
+import pytz
+
+class Disciplina(models.Model):
+
+     class Meta:
+        model = Disciplina
+        fields = '__all__'
+        widgets = {
+            'disciplina': TextInput (attrs={'class': "form-control"}),
+            }
+class Modo_Falha (models.Model):
+    model = Modo_Falha
+    fields = '__all__'
+    widgets = {
+        'disciplina': Select (attrs={'class': "form-control"}),
+        'modo_falha':TextInput (attrs={'class': "form-control"})
+        }
+    
+class Modo_falha_equipamento (models.Model):
+    model = Modo_falha_equipamento
+    fields = '__all__'
+    widgets = {
+        'modo_falha': Select (attrs={'class': "form-control"}),
+        'equipamento':Select (attrs={'class': "form-control"})
+        }
+class Nota_material (models.Model):
+    model = Nota_material
+    fields = '__all__'
+    widgets = {
+        'material': Select (attrs={'class': "form-control"}),
+        'quantidade':DecimalField (attrs={'class': "form-control"})
+        }
+    
+class Nota_equipamento (models.Model):
+    model = Nota_equipamento
+    fields = '__all__'
+    widgets = {
+        'titulo': TextInput (attrs={'class': "form-control"}),
+        'descricao':Textarea (attrs={'class': "form-control"}),
+        'equipamento':Select (attrs={'class': "form-control"}),
+        'modo_Falha_equipamento':Select (attrs={'class': "form-control"}),
+        'material':Select (attrs={'class': "form-control"}),
+        'data_ocorrencia':Textarea (attrs={'class': "form-control"}),
+        'falha': CheckboxInput (attrs={'class': "form-control"}),
+        'calibracao':CheckboxInput (attrs={'class': "form-control"}),
+        'lubrificao':CheckboxInput (attrs={'class': "form-control"}),
+        }
+    
