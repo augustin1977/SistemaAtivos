c1f045814d0f0df94ff439a630381bf262612bab
Eric Augsutin <ericaugustin@hotmail.com>
2023-03-08 13:52:57 -0300

    correção de bug de calibração

--- notas/forms.py
+++ notas/forms.py
@@ -82,6 +82,7 @@ class CadastraNota_equipamentoForm(ModelForm):
         utc=pytz.timezone(TIME_ZONE)
         cd['data_cadastro']=utc.localize( datetime.datetime.now())
         if (cd['calibracao']):
-               equipamento=Equipamento.objects.get(cd['equipamento'])
+               print('equipamento',cd['equipamento'])
+               equipamento=Equipamento.objects.get(id=cd['equipamento'].id)
                equipamento.data_ultima_calibracao=utc.localize( datetime.datetime.now())
                equipamento.save()   
