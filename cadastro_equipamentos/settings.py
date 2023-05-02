12e9a5859f770b9ae531842b3885ee73dcd53bba
Eric Augsutin <ericaugustin@hotmail.com>
2023-05-02 14:02:02 -0300

    ajustando

--- cadastro_equipamentos/settings.py
+++ cadastro_equipamentos/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 
 STATIC_URL = "/static"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    
     '/home/admin/SistemaAtivos/SistemaAtivos/static/']
-
+#os.path.join(BASE_DIR, 'static'),
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
