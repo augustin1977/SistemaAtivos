21e7bbdc35268c9542872ed376d8fb61f3afdeab
Eric Augustin <ericaugustin@hotmail.com>
2023-06-12 13:44:37 -0300

    ajustando pasta static

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,21 @@
+# gunicorn.conf.py
+
+import multiprocessing
+
+
 bind = "10.11.39.220:8000"
 logfile = "/home/admin/logs/gunicorn.log"
-workers = 3
+workers = multiprocessing.cpu_count() * 2 + 1  # Número de workers para processamento
+#timeout = 120  # Tempo limite de conexão em segundos
+
+# Configurações para servir arquivos estáticos
+def on_starting(server):
+    server._app_holder.settings.STATIC_ROOT = '/caminho/para/pasta/estatica/'
+
+    from django.core.management import call_command
+    call_command('collectstatic', interactive=False)
+
+    from whitenoise import WhiteNoise
+    server.application = WhiteNoise(server.application, root=server._app_holder.settings.STATIC_ROOT)
+
+# Outras configurações do Gunicorn...
