3615efc428dc8152aab01b17bdbf206b63acf15a
Eric Augustin <ericaugustin@hotmail.com>
2023-06-12 13:48:36 -0300

    ajustando pasta static

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,9 @@
 # gunicorn.conf.py
 
 import multiprocessing
+import os
+from cadastro_equipamentos.settings import BASE_DIR
+
 
 
 bind = "10.11.39.220:8000"
@@ -10,7 +13,7 @@ workers = multiprocessing.cpu_count() * 2 + 1  # Número de workers para process
 
 # Configurações para servir arquivos estáticos
 def on_starting(server):
-    server._app_holder.settings.STATIC_ROOT = '/caminho/para/pasta/estatica/'
+    server._app_holder.settings.STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     from django.core.management import call_command
     call_command('collectstatic', interactive=False)
