5db142721e26a8076065e18b43aa69490d360118
Eric Augustin <ericaugustin@hotmail.com>
2023-06-12 13:53:48 -0300

    ajustando pasta static

--- cadastro_equipamentos/urls.py
+++ cadastro_equipamentos/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('notas/', include('notas.urls')),
