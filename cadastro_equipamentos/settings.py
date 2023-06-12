e9685946c16c6a04198f89c8ad2c142a8db74871
Eric Augustin <ericaugustin@hotmail.com>
2023-06-12 10:08:31 -0300

    ajustando pasta static

--- cadastro_equipamentos/settings.py
+++ cadastro_equipamentos/settings.py
@@ -144,7 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static','icons')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static','icons'),
+                    os.path.join(BASE_DIR, 'cadastro_equipamentos','static'),
+                    os.path.join(BASE_DIR, 'cadastro_equipamentos','static','icons')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
