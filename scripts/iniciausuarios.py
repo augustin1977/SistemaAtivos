8453503a1abd5654e357c595359b0219d6f3ebd1
Eric Augustin <ericaugustin@hotmail.com>
2023-06-26 09:57:03 -0300

    ajustando a migração de login

--- scripts/iniciausuarios.py
+++ scripts/iniciausuarios.py
@@ -11,40 +11,40 @@ def run():
     print("importando dados do excel!")
     remetentes={}
     try:
-        nome_arquivo=os.path.join(BASE_DIR,"banco Migrado",'Lista_LPM.txt')
+        nome_arquivo=os.path.join(BASE_DIR,"banco Migrado",'Lista_LPM.txt') # abre o arquivo onde estão os dados do usuários
         
-        arquivo=open(nome_arquivo,"r")
+        arquivo=open(nome_arquivo,"r") # abre o oarquivo e pula as primieras linhas de cabeçalho
         linha=arquivo.readline()
         linha=arquivo.readline()
         linha=arquivo.readline()
         linha=arquivo.readline()
         i=0
         while(linha):
-            linha=arquivo.readline().strip()
+            linha=arquivo.readline().strip() # transforma a linha num vetor
             vetor=linha.split('\t')
             if vetor[0]:
                 remetentes[vetor[0]]=vetor[1]
-            i+=1
+            i+=1 # conta numero de linhas lidas
         arquivo.close()
     except Exception as erro:
-        print(erro)
-    pattern="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        print(erro) # imp´rime o erro em caso de erro
+    pattern="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$" # Padrão para verificar se o email é um valor válido
     tipo=Tipo.objects.filter(tipo="user")
+    sis=Usuario.objects.get(nome='System') # pega o usuario System
     #print(Tipo[0])
-    remetentes={"Eric Augustin":"ericaugustin@gmail.com"}
-    for nome in remetentes:
-        if nome and remetentes[nome]:
-            if re.search(pattern, remetentes[nome]):
-
-                senha=gera_senha(12)
-                senha_cripto=sha256(senha.encode()).hexdigest()
-                if len(Usuario.objects.filter(email=remetentes[nome]))==0:
+    #remetentes={"Eric Augustin":"ericaugustin@gmail.com"}
+    for nome in remetentes: # itera cada nome no dicionário remetentes
+        if nome and remetentes[nome]: # verifica se no dicionario está completa como nome e email
+            if re.search(pattern, remetentes[nome]): # verifica se o e-mail é valido
+                senha=gera_senha(12) # gera uma senha aleatoria de 12 digitos 
+                senha_cripto=sha256(senha.encode()).hexdigest() # criptografa a senha usado sha256
+                if len(Usuario.objects.filter(email=remetentes[nome]))==0: # verifica se o email ja está no banco de dados
                     usuario=Usuario(nome=nome,chapa=0,email=remetentes[nome],senha=senha_cripto,tipo=tipo[0],primeiro_acesso=True,ativo=True)
-                    usuario.save()
-                    sis=Usuario.objects.get(nome='System')
-                    Log.cadastramento(usuario,sis,'us')
-                    print(usuario,"-",remetentes[nome],'-',senha)
+                    usuario.save() # caso o usuario não exista, cadastra o usuario e salva no banco
                     
+                    Log.cadastramento(usuario,sis,'us') # grava o log de cadastro no sistema
+                    print(usuario,"-",remetentes[nome],'-',senha) # imprime usuario, email e senha na tela
+                    # texto do email em HTML
                     conteudo_html=f"""<html>
                                 <head></head>
                                 <body>
@@ -57,17 +57,19 @@ def run():
                                     <p>Obrigado!</p>
                                 </body>
                                 </html>"""
+                    # texto plain do email caso falhe o HTML
                     conteudo_plain=f"Seu cadastro foi concluido com sucesso, sua senha é {senha}"
                     try:
+                        # envia o email 
                         send_mail(subject='Cadastro no Sistema de Gestão de Ativos',message=conteudo_plain,
                             from_email="gestaodeativos@outlook.com.br",recipient_list=[usuario.email,info_email['email']],
                             html_message=conteudo_html)  
                         #time.sleep(random.randint(1,2)+random.randint(0,5))
                     except Exception as erro:
                         print("#######Erro no envio do email############")
-                        print(erro)
+                        print(erro) # em caso de erro imprime o erro
                 else:
-                    print(f"email {remetentes[nome]} já existe")
+                    print(f"email {remetentes[nome]} já existe") # caso o usuário ja exista imprime nome do usuario e informa que ja existe
             else:
                 print("##################Email incorreto##################")
         else:
