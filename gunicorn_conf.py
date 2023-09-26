5233a414c091dd63a6f79b24297613d112ce63f8
Eric Augustin <ericaugustin@hotmail.com>
2023-09-26 15:49:05 -0300

    ajustes

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,8 @@
 # gunicorn.conf.py
 
 import multiprocessing
-import os
-from cadastro_equipamentos.settings import BASE_DIR
+#import os
+#from cadastro_equipamentos.settings import BASE_DIR
 
 
 
@@ -10,5 +10,5 @@ bind = "10.11.39.220:8000"
 logfile = "/home/admin/logs/gunicorn.log"
 workers = multiprocessing.cpu_count() * 2 - 1  # Número de workers para processamento
 #timeout = 120  # Tempo limite de conexão em segundos
-
+chdir = "/home/admin/SistemaAtivos/SistemaAtivos"
 #
