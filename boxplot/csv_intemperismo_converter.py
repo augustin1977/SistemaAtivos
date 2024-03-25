a827ea46b31c2ac7dcc033830328a934de969f6d
Eric Augustin <ericaugustin@hotmail.com>
2024-03-25 16:41:07 -0300

    Ajuste do conversor do intemperisto sob pressão

--- boxplot/csv_intemperismo_converter.py
+++ boxplot/csv_intemperismo_converter.py
@@ -14,6 +14,7 @@ class Dados:
         self._temperatura=[]
         self._forca=[]
         self._troca_agua=[]
+        self._dataHora=[]
         
     def carrega_dados(self,dados):
         dados=dados.split("\n")
@@ -24,8 +25,11 @@ class Dados:
                     # print(dado)
                     # print(dado_aux)
                     self._record.append(int(dado_aux[0]))
-                    self._data.append(converte_data(dado_aux[1])),
-                    self._hora.append(dado_aux[2])
+                    data=converte_data(dado_aux[1])
+                    hora=dado_aux[2]
+                    self._data.append(data)
+                    self._hora.append(hora)
+                    self._dataHora.append(data+" "+hora)
                     self._temperatura.append(float(dado_aux[3]))
                     self._forca.append(float(dado_aux[4]))
                     self._troca_agua.append(int(dado_aux[5]))
@@ -34,6 +38,7 @@ class Dados:
         return {"record":self._record,
             "Data": self._data,
             "hora":self._hora,
+            "data_hora":self._dataHora,
             "temperatura":self._temperatura,
             "forca":self._forca,
             "troca_agua":self._troca_agua
