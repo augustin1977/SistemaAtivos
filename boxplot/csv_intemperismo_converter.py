32063448f11e598466a78aa6252d9c3913c9820a
Eric Augustin <ericaugustin@hotmail.com>
2024-03-26 16:52:52 -0300

    Ajuste do conversor do intemperisto sob pressão

--- boxplot/csv_intemperismo_converter.py
+++ boxplot/csv_intemperismo_converter.py
@@ -1,20 +1,19 @@
 import pandas as pd
 import boxplot.boxplot2
 from io import BytesIO
+from datetime import datetime
 
 def converte_data(dado):
-    data=dado.split("/")
-    return f"{data[1]}/{data[0]}/{data[2]}"
+   return datetime.strptime(dado,"%m/%d/%Y %H:%M:%S")
 
 class Dados:
     def __init__(self):
         self._record=[]
-        self._data=[]
-        self._hora=[]
+        self._dataHora=[]
         self._temperatura=[]
         self._forca=[]
         self._troca_agua=[]
-        #self._dataHora=[]
+        
         
     def carrega_dados(self,dados):
         dados=dados.split("\n")
@@ -23,23 +22,20 @@ class Dados:
             if i>0:
                 if dado:
                     self._record.append(int(dado_aux[0]))
-                    data=converte_data(dado_aux[1])
-                    hora=dado_aux[2]
-                    self._data.append(data)
-                    self._hora.append(hora)
-                    #self._dataHora.append(data+" "+hora)
+                    self._dataHora.append(converte_data(dado_aux[1]+" "+dado_aux[2]))
                     self._temperatura.append(float(dado_aux[3]))
                     self._forca.append(float(dado_aux[4]))
                     self._troca_agua.append(int(dado_aux[5]))
                 
     def retorna_dados(self):
-        return {"record":self._record,
-            "Data": self._data,
-            "hora":self._hora,
+        dados={"record":self._record,
+            "Data_Hora": self._dataHora,
             "temperatura":self._temperatura,
             "forca":self._forca,
             "troca_agua":self._troca_agua
             }
+        
+        return dados
 
     def retornaXLS(self):
         df=pd.DataFrame(self.retorna_dados())
@@ -54,5 +50,6 @@ def geraXLS(file):
     dados_raw=file.read()
     dados_raw=boxplot.boxplot2.try_decode(dados_raw)
     dados=Dados()
+    print(dados.retorna_dados())
     dados.carrega_dados(dados_raw)
     return dados.retornaXLS()
