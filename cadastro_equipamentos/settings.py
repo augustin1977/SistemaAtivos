deadbbadd1844101c9fd08cbfb9b02157c6eb2d3
Eric Augustin <ericaugustin@hotmail.com>
2024-05-02 09:47:52 -0300

    Ajustando para gerar chave hash

--- cadastro_equipamentos/settings.py
+++ cadastro_equipamentos/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"images/")
 SECRET_KEY = info_email.key 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["gestaoativosma.ad.ipt.br","10.3.10.20","10.11.39.220","127.0.0.1","10.3.10.32"]
 
