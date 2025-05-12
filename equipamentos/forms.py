5a300e2c28dd33800805ffb8dc302a1681df0a9a
Eric Augustin <ericaugustin@hotmail.com>
2025-05-12 11:53:06 -0300

    Ajuste bug

--- equipamentos/forms.py
+++ equipamentos/forms.py
@@ -226,14 +226,10 @@ class mediaForm(ModelForm):
         labels = {
             'nome': 'Nome do arquivo','equipamento':'Equipamento','documentos':'Documento - Tamanho máximo do arquivo - 40MB'
         }  
-    
-class cadastrarPermissaoForm (ModelForm):
-    equipamento = ModelChoiceField(queryset=Equipamento.objects.filter(ativo=True),widget=Select(attrs={'class': "form-control"}))
-    class Meta:
-        model = Autorizacao_equipamento
+    class cadastrarPermissaoForm (ModelForm):
+        equipamento = ModelChoiceField(queryset=Equipamento.objects.filter(ativo=True),widget=Select(attrs={'class': "form-control"}))
+        usuario = ModelChoiceField(queryset=Usuario.objects.filter(ativo=True),widget=Select(attrs={'class': "form-control"}))
+        class Meta:
+            model = Autorizacao_equipamento
+            fields = ['equipamento','usuario']
         
-        fields = ['equipamento','usuario']
-        widgets = {
-            'usuario': Select (attrs={'class': "form-control"}),
-            
-            }
