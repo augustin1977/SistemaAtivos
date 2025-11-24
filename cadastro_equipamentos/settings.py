5ab53f66137abb17e570325de0c90b1de526d062
Eric Augustin <ericaugustin@hotmail.com>
2025-11-24 10:37:55 -0300

    corrigindo erro no settings.py

--- cadastro_equipamentos/settings.py
+++ cadastro_equipamentos/settings.py
@@ -173,5 +173,5 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True # Encerra sessão ao fechar o browser
 
 # Verificando ambiente de testes
 
-AMBIENTE_TESTE = 'teste' in BASE_DIR
+AMBIENTE_TESTE = 'teste' in str(BASE_DIR)
 
